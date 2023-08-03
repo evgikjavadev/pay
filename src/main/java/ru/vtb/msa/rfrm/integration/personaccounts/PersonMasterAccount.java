7a290eb651810032be091c9f83db5ed84f08bc1d
Василенко Евгений Вадимович (4085285) <vasilenkoe@vtb.ru>
2023-08-03 06:07:42 +0300

    Работает сохранения полей в БД. Есть несколько todo.

--- src/main/java/ru/vtb/msa/rfrm/integration/personaccounts/PersonMasterAccount.java
+++
@@ -1,9 +0,0 @@
-//package ru.vtb.msa.rfrm.integration.personaccounts;
-//
-//
-///** Интерфейс для получения мастер счета клиентаиз ответа webClient */
-//public interface PersonMasterAccount {
-//
-//    String getRequestField(StringBuilder str, String strPattern);
-//
-//}
