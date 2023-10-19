17d7507119be54e6aa9f4f2fe491c586407f9a32
Василенко Евгений Вадимович (4085285) <vasilenkoe@vtb.ru>
2023-10-19 07:45:45 +0300

    Правка получение задания из топика rfrm_core_payment_order, сохранение в БД.

--- src/main/resources/db-changelog/changelog/add_column_reward_type.sql
+++ src/main/resources/db-changelog/changelog/add_column_reward_type.sql
@@ -1,4 +1,4 @@
 alter table rfrm_pay.ent_payment_task
-    add column reward_type integer NOT NULL;
+    add column reward_type integer;
 
 comment on column rfrm_pay.ent_payment_task.reward_type is 'Тип вознаграждения: 1 - Выплата (рубли), 2 - Выплата (бонусы)';
